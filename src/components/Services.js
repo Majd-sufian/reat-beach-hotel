6d8c1b134b6ca4e5c1778e2d1499a8bad6db6384
Majd Sufian <majdnewsufian@gmail.com>
2020-04-26 17:15:38 +0200

    adding sevices icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer, FaBreadSlice } from 'react-icons/fa'
 import Title from '../components/Title'
 
 
 export default class Services extends Component {
+    state={
+        services: [
+            {
+                icon:<FaCocktail/>,
+                title:'free Cocktails',
+                info: 'This guy is great for the practical side of things regarding React, and Vishwas at Codevolution is excellent for the fundamental'
+            }, 
+            {
+                icon:<FaHiking/>,
+                title:'Endless Hiking',
+                info: 'This guy is great for the practical side of things regarding React, and Vishwas at Codevolution is excellent for the fundamental'
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:'Free Shuttle',
+                info: 'This guy is great for the practical side of things regarding React, and Vishwas at Codevolution is excellent for the fundamental'
+            },
+            {
+                icon:<FaBreadSlice/>,
+                title:'Strongest Beer ',
+                info: 'This guy is great for the practical side of things regarding React, and Vishwas at Codevolution is excellent for the fundamental'
+            }
+        ]
+    }
+
+
     render() {
         return (
-            <div>
+            <section className="services">
                 <Title title="Services" />
-            </div>
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                    })}
+                </div>
+            </section>
         )
     }
 }
