ceb55f1ae48fc0d96488664be0daa1f0541e1865
Majd Sufian <majdnewsufian@gmail.com>
2020-04-27 23:59:07 +0200

    adding all the rooms to rooms page still working in search filtercontainer5

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useContext } from 'react' 
+import { RoomContext } from '../context'
+import Title from '../components/Title'
+import RoomContainer from './RoomContainer'
 
 export default function RoomFilter() {
+    const context = useContext(RoomContainer)
+    console.log(context)
     return (
         <div>
             Filter
