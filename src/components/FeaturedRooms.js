5cf15dfca3599d26eb61b7495846c8e665ef744e
Majd Sufian <majdnewsufian@gmail.com>
2020-04-27 14:07:10 +0200

    adding a loading gif and room comppenent

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { RoomContext } from '../context'
+import Loading from './Loading'
+import Room from './Room'
+import Title from './Title'
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
+        const { loading, featuredRooms: rooms } = this.context
+        console.log(rooms)
         return (
             <div>
-              test
+              featuredRooms 
+              <Room />
+              <Loading />
             </div>
         )
     }
